b13c3918bf0cc404b26904c91a51878988c8d066
Giorgi Tsartsidze <Tsartsidze.Giorgi@kiu.edu.ge>
2022-03-15 11:38:59 +0400

    Added MainBehaviour and Renderer classes (Renderer still needs fixing)

---
+++ src/main/java/MainBehaviour.java
@@ -0,0 +1,13 @@
+public interface MainBehaviour {
+
+
+    void start() throws Exception;
+
+    void input(Window window);
+
+    void update(float interval);
+
+    void render(Window window);
+
+
+}
