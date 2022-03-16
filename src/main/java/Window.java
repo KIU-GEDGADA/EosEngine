1ae54ca77ee2687f5112cca0b2ce913a3f9312c1
Giorgi Tsartsidze <Tsartsidze.Giorgi@kiu.edu.ge>
2022-03-16 18:54:39 +0400

    Added functions to Window class, for future implementations and testing

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,5 +1,8 @@
 public interface Window {
 
     public void start();
+    public boolean isRunning();
     public void update();
+    public void destroy();
+    public Boolean isVSyncActivated();
 }
