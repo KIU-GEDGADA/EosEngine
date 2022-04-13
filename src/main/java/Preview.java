6bed3e5fa6b7dcf66f8d42c7a05214421714d440
David <59766692+m3M0ryHuN73R@users.noreply.github.com>
2022-04-14 00:21:25 +0400

    Added index buffer rendering

--- src/main/java/Preview.java
+++ src/main/java/Preview.java
@@ -7,18 +7,16 @@ import graphics.*;
 import io.Input;
 import math.Vector3f;
 
-import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
 
 public class Preview {
     public static void main(String[] args) {
         Window w = new Window(800, 600, "Preview", false);
         Entity e = new Entity() {
-            Color r = new Color(1f, 0.0f, 0.0f, 1.0f);
-            Color g = new Color(0.0f, 1f, 0.0f, 1.0f);
-            Color b = new Color(0.0f, 0.0f, 1f, 1.0f);
             Vertex v1;
             Vertex v2;
             Vertex v3;
+            Vertex v4;
             Mesh m;
             Shader vs;
             Shader fs;
@@ -26,10 +24,12 @@ public class Preview {
 
             @Override
             public void init() {
-
-                v1 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), r);
-                v2 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), g);
-                v3 = new Vertex(new Vector3f(0f, 0.5f, 0f), b);
+                v1 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), Color.RED);
+                v2 = new Vertex(new Vector3f(-0.5f, 0.5f, 0f), Color.GREEN);
+                v3 = new Vertex(new Vector3f(0.5f, 0.5f, 0f), Color.BLUE);
+                v4 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), Color.YELLOW);
+                Vertex[] vertices = {v1, v2, v3, v4};
+                int[] indices = {2, 1, 0, 0, 1, 3};
                 vs = new Shader("res/shaders/v3vs.glsl");
                 vs.compile();
                 fs = new Shader("res/shaders/v3fs.glsl");
@@ -39,14 +39,14 @@ public class Preview {
                 sp.attachShader(vs);
                 sp.attachShader(fs);
                 sp.link();
-                m = new Mesh(new Vertex[]{v1, v2, v3});
+                m = new Mesh(vertices, indices);
                 m.init();
                 sp.bind();
             }
 
             @Override
             public void update() {
-                if(Input.isKeyPressed(GLFW_KEY_SPACE)) {
+                if (Input.isKeyPressed(GLFW_KEY_SPACE)) {
                     System.out.println("Space pressed");
                 }
             }
