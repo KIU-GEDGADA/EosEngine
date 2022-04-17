eb59a0f2ad3c88c9c997be1ba1fe52b55c817cfa
adamkaram1 <karam.adam@kiu.edu.ge>
2022-04-17 23:29:30 +0400

    Implemented Vector4f Testing

---
+++ src/test/java/math/Vector4fTest.java
@@ -0,0 +1,13 @@
+package math;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Vector4fTest {
+    Vector4f v1 = new Vector4f(2,2,2,2);
+    Vector4f v2 = new Vector4f(3,3,3,3);
+    @Test
+    void normalize(){
+        Vector4f testNormalizedVector = new Vector4f(0.5f,0.5f,0.5f,0.5f);
+        assertEquals(testNormalizedVector,Vector4f.normalize(v1));
+    }
+}
