1976269082d2ab09020c788be62b368984e80be9
adamkaram1 <karam.adam@kiu.edu.ge>
2022-05-08 20:22:10 +0400

    Fixed function name(s) to reflect testing

--- src/test/java/math/Vector4fTest.java
+++ src/test/java/math/Vector4fTest.java
@@ -6,7 +6,7 @@ public class Vector4fTest {
     Vector4f v1 = new Vector4f(2,2,2,2);
     Vector4f v2 = new Vector4f(3,3,3,3);
     @Test
-    void normalize(){
+    void testNormalize(){
         Vector4f testNormalizedVector = new Vector4f(0.5f,0.5f,0.5f,0.5f);
         assertEquals(testNormalizedVector,Vector4f.normalize(v1));
     }
