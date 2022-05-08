324760e691a3aa25097bfc128d9452540f431179
adamkaram1 <karam.adam@kiu.edu.ge>
2022-05-08 20:35:30 +0400

    Added init

--- src/test/java/math/Vector4fTest.java
+++ src/test/java/math/Vector4fTest.java
@@ -1,10 +1,17 @@
 package math;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Vector4fTest {
-    Vector4f v1 = new Vector4f(2,2,2,2);
-    Vector4f v2 = new Vector4f(3,3,3,3);
+    Vector4f v1;
+    Vector4f v2;
+    @BeforeEach
+    void init(){
+        Vector4f v1 = new Vector4f(2,2,2,2);
+        Vector4f v2 = new Vector4f(3,3,3,3);
+    }
+
     @Test
     void testNormalize(){
         Vector4f testNormalizedVector = new Vector4f(0.5f,0.5f,0.5f,0.5f);
