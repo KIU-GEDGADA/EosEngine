79b447a4e59cac6d9c5c8d4cd3548a2b3f9356f4
adamkaram1 <karam.adam@kiu.edu.ge>
2022-05-08 21:43:56 +0400

    Added more tests

--- src/test/java/math/Vector3fTest.java
+++ src/test/java/math/Vector3fTest.java
@@ -17,13 +17,23 @@ public class Vector3fTest {
         v3 = new Vector3f(2, 2, 1);
     }
 
-
+    @Test
+    void testNonStaticAdd(){
+        Vector3f additionTestVector = new Vector3f(9,5,11);
+        v1.add(v2);
+        assertEquals(additionTestVector,v1);
+    }
     @Test
     void testStaticAdd() {
         Vector3f additionTestVector = new Vector3f(9, 5, 11);
         assertEquals(additionTestVector, Vector3f.add(v1, v2));
     }
-
+    @Test
+    void testNonStaticSub(){
+        Vector3f subtractionTestVector = new Vector3f(5,1,7);
+        v2.sub(v1);
+        assertEquals(subtractionTestVector,v2);
+    }
     @Test
     void testStaticSub() {
         Vector3f subtractionTestVector = new Vector3f(5, 1, 7);
@@ -34,5 +44,27 @@ public class Vector3fTest {
     void testLength() {
         float correctLength = 3;
         assertEquals(correctLength, v3.length());
+    }
+    @Test
+    void testNonStaticMul(){
+        Vector3f mulTestVector = new Vector3f(4,4,4);
+        v1.mul(2);
+        assertEquals(mulTestVector,v1);
+    }
+    @Test
+    void testStaticMul(){
+        Vector3f mulTestVector = new Vector3f(4,4,4);
+        assertEquals(mulTestVector,Vector3f.mul(v1,2));
+    }
+    @Test
+    void testNonStaticDiv(){
+        Vector3f divTestVector = new Vector3f(1,1,1);
+        v1.div(2);
+        assertEquals(divTestVector,v1);
+    }
+    @Test
+    void testStaticDiv(){
+        Vector3f divTestVector = new Vector3f(1,1,1);
+        assertEquals(divTestVector,Vector3f.div(v1,2));
     }
 }
