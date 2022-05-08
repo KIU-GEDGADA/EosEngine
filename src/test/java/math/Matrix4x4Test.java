a0734188df49ca31b60a638182e035b143b34fc5
adamkaram1 <karam.adam@kiu.edu.ge>
2022-05-08 17:28:08 +0400

    Changed function names to reflect testing

--- src/test/java/math/Matrix4x4Test.java
+++ src/test/java/math/Matrix4x4Test.java
@@ -18,7 +18,7 @@ public class Matrix4x4Test {
             {3,1,5,6}
     });
     @Test
-    void multiply(){
+    void testMultiply(){
         Matrix4x4 testMultMatrix = new Matrix4x4(new float[][]{
                 {6,2,6,6},
                 {10,2,3,5},
@@ -28,7 +28,7 @@ public class Matrix4x4Test {
         assertEquals(testMultMatrix,m1.multiply(m2));
     }
     @Test
-    void transpose(){
+    void testTranspose(){
         Matrix4x4 testTransMatrix = new Matrix4x4(new float[][]{
                 {1,2,3,0},
                 {0,0,0,1},
@@ -38,7 +38,7 @@ public class Matrix4x4Test {
         assertEquals(testTransMatrix,m1.transpose());
     }
     @Test
-    void setIdentity(){
+    void testSetIdentity(){
         Matrix4x4 testIdentityMatrix = new Matrix4x4(new float[][]{
                 {1,0,0,0},
                 {0,1,0,0},
