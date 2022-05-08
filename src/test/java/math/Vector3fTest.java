715e8d5762fbca5af47e3421bdd5593a838def58
adamkaram1 <karam.adam@kiu.edu.ge>
2022-05-08 21:15:26 +0400

    Added init for vector3ftest

--- src/test/java/math/Vector3fTest.java
+++ src/test/java/math/Vector3fTest.java
@@ -1,27 +1,38 @@
 package math;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Vector3fTest {
-    Vector3f v1 = new Vector3f(2,2,2);
-    Vector3f v2 = new Vector3f(7,3,9);
-    Vector3f v3 = new Vector3f(2,2,1);
+    Vector3f v1;
+    Vector3f v2;
+    Vector3f v3;
+
+    @BeforeEach
+    void init() {
+        v1 = new Vector3f(2, 2, 2);
+        v2 = new Vector3f(7, 3, 9);
+        v3 = new Vector3f(2, 2, 1);
+    }
+
 
     @Test
-    void add(){
-        Vector3f additionTestVector = new Vector3f(9,5,11);
-        assertEquals(additionTestVector,Vector3f.add(v1,v2));
+    void add() {
+        Vector3f additionTestVector = new Vector3f(9, 5, 11);
+        assertEquals(additionTestVector, Vector3f.add(v1, v2));
     }
+
     @Test
-    void sub(){
-        Vector3f subtractionTestVector = new Vector3f(5,1,7);
-        assertEquals(subtractionTestVector,Vector3f.sub(v2,v1));
+    void sub() {
+        Vector3f subtractionTestVector = new Vector3f(5, 1, 7);
+        assertEquals(subtractionTestVector, Vector3f.sub(v2, v1));
     }
+
     @Test
-    void length(){
+    void length() {
         float correctLength = 3;
-        assertEquals(correctLength,v3.length());
+        assertEquals(correctLength, v3.length());
     }
 }
