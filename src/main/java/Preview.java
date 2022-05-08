c496f046fdb820ecebba9e3ff916e64bd45bc692
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-05-08 21:08:11 +0400

    Added multi-mesh rendering to test Renderer class

--- src/main/java/Preview.java
+++ src/main/java/Preview.java
@@ -1,8 +1,6 @@
 //import core.Renderer;
 
-import core.Engine;
-import core.Entity;
-import core.Window;
+import core.*;
 import graphics.*;
 import io.Input;
 import math.Vector3f;
@@ -12,24 +10,28 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
 public class Preview {
     public static void main(String[] args) {
         Window w = new Window(800, 600, "Preview", false);
+        Vertex v1;
+        Vertex v2;
+        Vertex v3;
+        Vertex v4;
+        v1 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), Color.RED);
+        v2 = new Vertex(new Vector3f(-0.5f, 0.5f, 0f), Color.GREEN);
+        v3 = new Vertex(new Vector3f(0.5f, 0.5f, 0f), Color.BLUE);
+        v4 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), Color.YELLOW);
+        Vertex[] vertices1 = {v1, v2, v3};
+        Vertex[] vertices2 = {v1, v2, v4};
+        int[] indices1 = {2, 1, 0};
+        int[] indices2 = {0, 1, 2};
+        Item item1 = new Item("Mesh 1", new Mesh(vertices1, indices1));
+        Item item2 = new Item("Mesh 2", new Mesh(vertices2, indices2));
         Entity e = new Entity() {
-            Vertex v1;
-            Vertex v2;
-            Vertex v3;
-            Vertex v4;
-            Mesh m;
             Shader vs;
             Shader fs;
             ShaderProgram sp;
 
+
             @Override
             public void init() {
-                v1 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), Color.RED);
-                v2 = new Vertex(new Vector3f(-0.5f, 0.5f, 0f), Color.GREEN);
-                v3 = new Vertex(new Vector3f(0.5f, 0.5f, 0f), Color.BLUE);
-                v4 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), Color.YELLOW);
-                Vertex[] vertices = {v1, v2, v3, v4};
-                int[] indices = {2, 1, 0, 0, 1, 3};
                 vs = new Shader("res/shaders/v3vs.glsl");
                 vs.compile();
                 fs = new Shader("res/shaders/v3fs.glsl");
@@ -39,9 +41,7 @@ public class Preview {
                 sp.attachShader(vs);
                 sp.attachShader(fs);
                 sp.link();
-                m = new Mesh(vertices, indices);
-                m.init();
-                sp.bind();
+                Renderer.setShader(sp);
             }
 
             @Override
@@ -53,16 +53,16 @@ public class Preview {
 
             @Override
             public void render() {
-                m.render();
             }
 
             @Override
             public void destroy() {
-                sp.destroy();
-                m.destroy();
             }
         };
-        Engine ee = new Engine(w, e);
+        Engine ee = new Engine(w);
+        ee.entities.add(e);
+        Renderer.addItem(item1);
+        Renderer.addItem(item2);
         ee.start();
     }
 }
