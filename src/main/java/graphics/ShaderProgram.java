bbe10e3f9fb760e1ce831c756a65db2dfec98a96
theInfernofire <74895573+theInfernofire@users.noreply.github.com>
2022-05-09 18:06:12 +0400

    Moved flatten and matrixToFloatbuffer functions to MathUtils

--- src/main/java/graphics/ShaderProgram.java
+++ src/main/java/graphics/ShaderProgram.java
@@ -4,6 +4,7 @@ import math.Matrix4x4;
 import math.Vector2f;
 import math.Vector3f;
 import math.Vector4f;
+import utils.MathUtils;
 
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
@@ -141,7 +142,7 @@ public class ShaderProgram {
     }
     public void setUniformm4f(String name, Matrix4x4 value){
         int location = uniforms.get(name);
-        FloatBuffer buffer = FloatBuffer.wrap(value.flatten());
+        FloatBuffer buffer = MathUtils.matrixToFloatBuffer(value);
         if(location != -1){
             glUniform4fv(location,buffer);
         }
