12765669730fa95ce375a064452be08373e7d133
adamkaram1 <karam.adam@kiu.edu.ge>
2022-05-09 19:29:06 +0400

    Completed all matrix4x4 tests

--- src/test/java/math/Matrix4x4Test.java
+++ src/test/java/math/Matrix4x4Test.java
@@ -125,4 +125,27 @@ public class Matrix4x4Test {
         });
         assertEquals(inverseTestMatrix,m1.inverse());
     }
+    @Test
+    void testSetMatrix(){
+        Matrix4x4 setMatrixTest = new Matrix4x4(new float[][]{
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        });
+        m1.setMatrix(new float[][]{{1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}});
+        assertEquals(setMatrixTest,m1);
+    }
+    @Test
+    void testMultiplyScalar(){
+        Matrix4x4 scalarMulTest = new Matrix4x4(new float[][]{{2, 0, 0, 2},
+                {4, 0, 2, 0},
+                {6, 0, 10, 0},
+                {0, 2, 4, 4}});
+        m1.multiply(2);
+        assertEquals(scalarMulTest,m1);
+    }
 }
