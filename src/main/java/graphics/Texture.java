f72980aa844658b315ff5910b68347ed97e31fb5
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-05-19 17:39:38 +0400

    Changed RGBA to RGB for proper loading

--- src/main/java/graphics/Texture.java
+++ src/main/java/graphics/Texture.java
@@ -5,46 +5,49 @@ import org.lwjgl.BufferUtils;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
-import static org.lwjgl.opengl.GL11C.*;
-import static org.lwjgl.stb.STBImage.*;
+import static org.lwjgl.opengl.GL33.*;
+import static org.lwjgl.stb.STBImage.stbi_image_free;
+import static org.lwjgl.stb.STBImage.stbi_load;
 
 public class Texture {
-    private String filepath;
+    private final String filepath;
     private int id;
 
-    public Texture(String filepath){
+    public Texture(String filepath) {
         this.filepath = filepath;
+    }
 
+    public void init() {
         id = glGenTextures();
-        glBindTexture(GL_TEXTURE_2D,id);
+        glBindTexture(GL_TEXTURE_2D, id);
 
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
         IntBuffer width = BufferUtils.createIntBuffer(1);
         IntBuffer height = BufferUtils.createIntBuffer(1);
         IntBuffer channels = BufferUtils.createIntBuffer(1);
-        ByteBuffer image = stbi_load(filepath,width,height,channels,0);
+        ByteBuffer image = stbi_load(filepath, width, height, channels, 0);
 
-        if(image != null){
-            glTexImage2D(GL_TEXTURE_2D,0, GL_RGBA, width.get(0),height.get(0),0,GL_RGBA,GL_UNSIGNED_BYTE, image);
+        if (image != null) {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width.get(0), height.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, image);
 
-        }else{
+        } else {
             assert false : "Error: could not load image" + filepath + " ; ";
         }
 
         stbi_image_free(image);
     }
 
-    public void bind(){
-        glBindTexture(GL_TEXTURE_2D,id);
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
     }
 
-    public void unBind(){
-        glBindTexture(GL_TEXTURE_2D,0);
+    public void unbind() {
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 
 }
