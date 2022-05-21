d3b22310f5fa49287618f3db96a1794d94898a5a
gioke <giokepa4@gmaill.com>
2022-05-22 00:20:27 +0400

    Demo: transformation working

--- src/main/java/Preview.java
+++ src/main/java/Preview.java
@@ -11,38 +11,41 @@ import static org.lwjgl.glfw.GLFW.GLFW_KEY_SPACE;
 public class Preview {
     public static void main(String[] args) {
         Window w = new Window(1000, 1000, "Preview", false);
-        Vertex v1;
-        Vertex v2;
-        Vertex v3;
-        Vertex v4;
-        v1 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), Color.RED);
-        v2 = new Vertex(new Vector3f(-0.5f, 0.5f, 0f), Color.GREEN);
-        v3 = new Vertex(new Vector3f(0.5f, 0.5f, 0f), Color.BLUE);
-        v4 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), Color.YELLOW);
-        Vertex[] vertices1 = {v1, v2, v3};
-        Vertex[] vertices2 = {v1, v2, v4};
-        int[] indices1 = {2, 1, 0};
-        int[] indices2 = {0, 1, 2};
-        Shader vs = new Shader("res/shaders/v3vs.glsl");
-        Shader fs = new Shader("res/shaders/v3fs.glsl");
-        List<Shader> shaders = new ArrayList<>();
-        shaders.add(vs);
-        shaders.add(fs);
-        Item item1 = new Item("Mesh 1", new Mesh(vertices1, indices1), shaders);
-        Item item2 = new Item("Mesh 2", new Mesh(vertices2, indices2), shaders);
-        Item item3 = new Item("Cube", new Mesh("res/models/gordon.obj", false), shaders);
 
         Entity e = new Entity() {
+            Item item1;
+            Item item3;
+            Item item2;
 
             @Override
             public void init() {
+                Vertex v1;
+                Vertex v2;
+                Vertex v3;
+                Vertex v4;
+                v1 = new Vertex(new Vector3f(-0.5f, 0.5f, 0f), Color.RED);
+                v2 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), Color.GREEN);
+                v3 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), Color.BLUE);
+                v4 = new Vertex(new Vector3f(0.5f, 0.5f, 0f), Color.YELLOW);
+                Vertex[] vertices1 = {v1, v2, v3, v4};
+                int[] indices1 = {0, 1, 3, 3, 1, 2};
+                Shader vs = new Shader("res/shaders/v3vs.glsl");
+                Shader fs = new Shader("res/shaders/v3fs.glsl");
+                List<Shader> shaders = new ArrayList<>();
+                shaders.add(vs);
+                shaders.add(fs);
+                item1 = new Item("Mesh 1", new Mesh(vertices1, indices1), shaders);
+                item3 = new Item("Cube", new Mesh("res/models/gordon.obj", false), shaders);
+
+                Renderer.addItem(item3);
             }
 
             @Override
             public void update() {
-                if (Input.isKeyPressed(GLFW_KEY_SPACE)) {
-                    System.out.println("Space pressed");
+                if (item3.getTransform().getRotation().y < -360f) {
+                    item3.getTransform().getRotation().y = 0f;
                 }
+                item3.getTransform().getRotation().y -= 1f;
             }
 
             @Override
@@ -55,7 +58,6 @@ public class Preview {
         };
         Engine ee = new Engine(w);
         ee.getEntities().add(e);
-        Renderer.addItem(item3);
         ee.start();
     }
 }
