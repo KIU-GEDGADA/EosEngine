863fa5e4ca67a7d64eed6d1c9fd934c4eec85701
gioke <giokepa4@gmaill.com>
2022-05-21 21:54:37 +0400

    Fixed refactoring in Transform.java

--- src/main/java/math/Transform.java
+++ src/main/java/math/Transform.java
@@ -1,30 +1,36 @@
 package math;
 
+import core.Item;
+
 public class Transform {
-    private Vector3f translation;
+    private Vector3f position;
     private Vector3f rotation;
     private Vector3f scale;
 
-    public Transform(){
-        translation = new Vector3f();
+    public Transform() {
+        position = new Vector3f();
         rotation = new Vector3f();
-        scale = new Vector3f(1.0f,1.0f,1.0f);
+        scale = Vector3f.one();
     }
-    public Vector3f getTranslation(){
-        return translation;
+
+    public Vector3f getPosition() {
+        return position;
     }
-    public void setTranslation(Vector3f v1){
-        translation = v1;
+
+    public void setPosition(Vector3f v1) {
+        position = v1;
     }
-    public void setTranslation(float x, float y, float z){
-        translation.x = x;
-        translation.y = y;
-        translation.z = z;
+
+    public void setPosition(float x, float y, float z) {
+        position.x = x;
+        position.y = y;
+        position.z = z;
     }
-    public Matrix4x4 getTransformation(){
-        Matrix4x4 translationMatrix = MatrixHelper.getTranslationMatrix(translation.x,translation.y,translation.z);
-        Matrix4x4 rotationMatrix = MatrixHelper.getRotationMatrix(rotation.x,rotation.y,rotation.z);
-        Matrix4x4 scaleMatrix = MatrixHelper.getScaleMatrix(scale.x,scale.y,scale.z);
+
+    public Matrix4x4 getTransformation() {
+        Matrix4x4 translationMatrix = MatrixHelper.getTranslationMatrix(position.x, position.y, position.z);
+        Matrix4x4 rotationMatrix = MatrixHelper.getRotationMatrix(rotation.x, rotation.y, rotation.z);
+        Matrix4x4 scaleMatrix = MatrixHelper.getScaleMatrix(scale.x, scale.y, scale.z);
         return translationMatrix.multiply(rotationMatrix.multiply(scaleMatrix));
     }
 }
