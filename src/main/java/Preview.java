0b9f5aefb9b0eb24041f1f455375db3dfb44b952
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2022-05-22 17:41:09 +0400

    Preview with switchable 2D/3D models, without textures working

--- src/main/java/Preview.java
+++ src/main/java/Preview.java
@@ -1,6 +1,7 @@
 import core.*;
 import graphics.*;
 import io.Input;
+import math.Vector2f;
 import math.Vector3f;
 
 import java.util.ArrayList;
@@ -14,38 +15,42 @@ public class Preview {
 
         Entity e = new Entity() {
             Item item1;
-            Item item3;
             Item item2;
-
+            Texture texture1;
             @Override
             public void init() {
-                Vertex v1;
-                Vertex v2;
-                Vertex v3;
-                Vertex v4;
-                v1 = new Vertex(new Vector3f(-0.5f, 0.5f, 0f), Color.RED);
-                v2 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), Color.GREEN);
-                v3 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), Color.BLUE);
-                v4 = new Vertex(new Vector3f(0.5f, 0.5f, 0f), Color.YELLOW);
+                Vertex v1 = new Vertex(new Vector3f(-0.5f, 0.5f, 0f), Color.RED);
+                Vertex v2 = new Vertex(new Vector3f(-0.5f, -0.5f, 0f), Color.GREEN);
+                Vertex v3 = new Vertex(new Vector3f(0.5f, -0.5f, 0f), Color.BLUE);
+                Vertex v4 = new Vertex(new Vector3f(0.5f, 0.5f, 0f), Color.YELLOW);
+
                 Vertex[] vertices1 = {v1, v2, v3, v4};
                 int[] indices1 = {0, 1, 3, 3, 1, 2};
-                Shader vs = new Shader("res/shaders/v3vs.glsl");
-                Shader fs = new Shader("res/shaders/v3fs.glsl");
-                List<Shader> shaders = new ArrayList<>();
-                shaders.add(vs);
-                shaders.add(fs);
-                item1 = new Item("Mesh 1", new Mesh(vertices1, indices1), shaders);
-                item3 = new Item("Cube", new Mesh("res/models/gordon.obj", false), shaders);
+                Vector2f[] texCoords1 = {new Vector2f(0, 1), new Vector2f(0, 1), new Vector2f(1, 0), new Vector2f(1, 0)};
 
-                Renderer.addItem(item3);
+                Shader vs = new Shader("res/shaders/tv2vs.glsl");
+                Shader fs = new Shader("res/shaders/tv2fs.glsl");
+                List<Shader> shaders = List.of(new Shader[]{vs, fs});
+
+                texture1 = new Texture("res/textures/texture.png");
+
+                item1 = new Item("Mesh 1", new Model(new Mesh(vertices1, texCoords1, new Vector3f[]{}, indices1)), shaders);
+
+                item2 = new Item("Cube", new Model(new Mesh("res/models/cube.obj")), shaders);
+                item2.getTransform().getScale().div(2f);
+                //item1.getModel().setTexture(texture1);
+
+                Renderer.addItem(item1);
+                //Renderer.addItem(item2);
             }
 
             @Override
             public void update() {
-                if (item3.getTransform().getRotation().y < -360f) {
-                    item3.getTransform().getRotation().y = 0f;
+
+                if (item1.getTransform().getRotation().z < -360f) {
+                    item1.getTransform().getRotation().z = 0f;
                 }
-                item3.getTransform().getRotation().y -= 1f;
+                item1.getTransform().getRotation().z -= 1f;
             }
 
             @Override
@@ -54,10 +59,14 @@ public class Preview {
 
             @Override
             public void destroy() {
+                item1.destroy();
+                //item2.destroy();
             }
         };
+
         Engine ee = new Engine(w);
         ee.getEntities().add(e);
+
         ee.start();
     }
 }
