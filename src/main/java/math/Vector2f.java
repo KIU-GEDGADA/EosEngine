ecb4acfdc154a400e630e8f3174ede2852977c0c
gioke <giokepa4@gmaill.com>
2022-05-23 11:17:34 +0400

    added equals function

--- src/main/java/math/Vector2f.java
+++ src/main/java/math/Vector2f.java
@@ -159,4 +159,12 @@ public class Vector2f {
         return new float[]{x, y};
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector2f vector2f = (Vector2f) o;
+        return Float.compare(vector2f.x, x) == 0 && Float.compare(vector2f.y, y) == 0;
+    }
+
 }
