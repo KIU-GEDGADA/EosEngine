b211386c72397370cfa98bdf51d4b0627b442140
gioke <giokepa4@gmaill.com>
2022-06-10 16:44:12 +0400

    Added projections and getters/setters to Camera class.

--- src/main/java/core/Camera.java
+++ src/main/java/core/Camera.java
@@ -7,15 +7,59 @@ public class Camera {
 
     private Vector3f position;
     private Vector3f rotation;
+    private float FOV;
+    private float zFar;
+    private float zNear;
+    private float aspectRatio;
 
-    public Camera() {
+    public Camera(float aspectRatio) {
         position = Vector3f.zero();
         rotation = Vector3f.zero();
+        this.aspectRatio = aspectRatio;
+        zFar = 1000;
+        zNear = 1;
+        FOV = 60;
     }
 
-    public Camera(Vector3f position, Vector3f rotation) {
+    public Camera(Vector3f position, Vector3f rotation, float FOV, float zFar, float zNear, float aspectRatio) {
         this.position = position;
         this.rotation = rotation;
+        this.FOV = FOV;
+        this.zFar = zFar;
+        this.zNear = zNear;
+        this.aspectRatio = aspectRatio;
+    }
+
+    public float getFOV() {
+        return FOV;
+    }
+
+    public void setFOV(float FOV) {
+        this.FOV = FOV;
+    }
+
+    public float getzFar() {
+        return zFar;
+    }
+
+    public void setzFar(float zFar) {
+        this.zFar = zFar;
+    }
+
+    public float getzNear() {
+        return zNear;
+    }
+
+    public void setzNear(float zNear) {
+        this.zNear = zNear;
+    }
+
+    public float getAspectRatio() {
+        return aspectRatio;
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        this.aspectRatio = aspectRatio;
     }
 
     public Vector3f getPosition() {
