ef5f054bbd911f7432d1e67f251737db2408b85f
gioke <giokepa4@gmaill.com>
2022-06-12 13:48:34 +0400

    Added MOUSE_SENSITIVY Constant

--- src/main/java/enums/Constants.java
+++ src/main/java/enums/Constants.java
@@ -9,5 +9,6 @@ public enum Constants {
     public static final int WIDTH = 1000;
     public static final int HEIGHT = 1000;
     public static final int FPS = 60;
+    public static final float MOUSE_SENSITIVITY = 0.2f;
 
 }
