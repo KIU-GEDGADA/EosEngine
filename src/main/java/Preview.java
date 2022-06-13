7d7694515976cf301a448dd5f7f6716defda43d7
gioke <giokepa4@gmaill.com>
2022-06-13 13:17:07 +0400

    Fixed refactoring issues

--- src/main/java/Preview.java
+++ src/main/java/Preview.java
@@ -6,10 +6,10 @@ import static enums.Constants.*;
 public class Preview {
     public static void main(String[] args) {
 
-        Window w = Window.getInstance(WIDTH, HEIGHT, "Preview", false);
-        DummyGame e = new DummyGame();
-        Engine ee = Engine.getInstance(w);
-        ee.getEntities().add(e);
-        ee.start();
+        Window window = Window.getInstance(WIDTH, HEIGHT, "Preview", false);
+        DummyGame game = new DummyGame();
+        Engine engine = Engine.getInstance(window);
+        engine.getEntities().add(game);
+        engine.start();
     }
 }
