057f23704207229d5982793260de0374df08292f
theInfernofire <74895573+theInfernofire@users.noreply.github.com>
2022-06-15 03:35:45 +0400

    Made corrections in JavaDoc comments

--- src/main/java/core/Entity.java
+++ src/main/java/core/Entity.java
@@ -4,11 +4,23 @@ package core;
  * The interface of an entity object of the game engine, every object to be handled and rendered should implement this interface
  */
 public interface Entity {
+    /**
+     * Initialization function
+     */
     void init();
 
+    /**
+     * Update functions
+     */
     void update();
 
+    /**
+     * Render function
+     */
     void render();
 
+    /**
+     * Destroy function
+     */
     void destroy();
 }
