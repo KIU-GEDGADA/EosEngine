e1cc99d243f67273fed1a6d704858071dfdc1b78
giokepa <giokepa4@gmail.com>
2022-08-27 11:55:24 +0400

    Added ambientLight and material to shader

--- res/shaders/tv2fs.glsl
+++ res/shaders/tv2fs.glsl
@@ -1,15 +1,38 @@
 //type fragment
 #version 450
 
-uniform sampler2D texSampler;
-uniform bool useTexture;
 
 in vec2 fTexCords;
+in vec3 fNormal;
+in vec3 fPos;
 in vec4 fColor;
 
 out vec4 fragColor;
 
+struct Material {
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    int hasTexture;
+    float reflectance;
+};
+
+uniform sampler2D texSampler;
+uniform vec3 ambientLight;
+uniform Material material;
+uniform bool useTexture;
+
+
+vec4 ambientC;
+
+
 void main()
 {
-    fragColor = useTexture ? texture(texSampler, fTexCords) : fColor;
+
+    if(material.hasTexture == 1) {
+        ambientC = texture(texSampler, fTexCords);
+    } else {
+        ambientC = material.ambient + material.specular + material.diffuse + material.reflectance;
+    }
+    fragColor = ambientC * vec4(ambientLight, 1);
 }
--- res/shaders/tv2vs.glsl
+++ res/shaders/tv2vs.glsl
@@ -3,16 +3,25 @@
 layout (location=0) in vec3 aPos;
 layout (location=1) in vec4 aColor;
 layout (location=2) in vec2 aTexCords;
+layout (location=3) in vec3 normal;
 
 out vec2 fTexCords;
 out vec4 fColor;
+out vec3 fNormal;
+out vec3 fPos;
 
 uniform mat4 tMat;
 uniform mat4 vMat;
 uniform mat4 pMat;
 
 void main(){
+
+    vec4 worldPos = tMat * vec4(aPos, 1.0f);
+    gl_Position = pMat * vMat * worldPos;
+
+
+    fNormal = normalize(worldPos).xyz;
+    fPos = worldPos.xyz;
     fColor = aColor;
     fTexCords = aTexCords;
-    gl_Position = pMat * vMat * tMat * vec4(aPos, 1.0f);
 }
