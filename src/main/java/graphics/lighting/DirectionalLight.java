9464fc7091335e7560681810cf0f28e83c2afc45
giokepa <giokepa4@gmail.com>
2022-08-30 14:49:48 +0400

    Added DirectionalLight

---
+++ src/main/java/graphics/lighting/DirectionalLight.java
@@ -0,0 +1,39 @@
+package graphics.lighting;
+
+import math.Vector3f;
+
+public class DirectionalLight {
+
+    private Vector3f color, direction;
+    private float intensity;
+
+    public DirectionalLight(Vector3f color, Vector3f direction, float intensity) {
+        this.color = color;
+        this.direction = direction;
+        this.intensity = intensity;
+    }
+
+    public Vector3f getColor() {
+        return color;
+    }
+
+    public void setColor(Vector3f color) {
+        this.color = color;
+    }
+
+    public Vector3f getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Vector3f direction) {
+        this.direction = direction;
+    }
+
+    public float getIntensity() {
+        return intensity;
+    }
+
+    public void setIntensity(float intensity) {
+        this.intensity = intensity;
+    }
+}
