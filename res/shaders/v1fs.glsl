58127a6ed6c924050540c85dc2bc3b11debe2e5d
giokepa <giokepa4@gmail.com>
2022-08-31 22:22:47 +0400

    Deleted unnecessary Shaders

--- res/shaders/v1fs.glsl
+++
@@ -1,9 +0,0 @@
-//type fragment
-#version 330
-
-out vec4 fragColor;
-
-void main()
-{
-    fragColor = vec4(0.0, 1.0, 0.5, 1.0);
-}
--- res/shaders/v1vs.glsl
+++
@@ -1,9 +0,0 @@
-//type vertex
-#version 330
-
-layout (location = 0) in vec3 position;
-
-void main()
-{
-    gl_Position = vec4(0.25 * position, 1.0);
-}
